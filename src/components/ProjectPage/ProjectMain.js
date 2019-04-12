fed9aeb73715c89e64bfbc036e996844673128e3
kashsid <kashifsiddique@gmail.com>
2019-04-12 15:59:16 -0500

    Adds header layout on main project page

--- src/components/ProjectPage/ProjectMain.js
+++ src/components/ProjectPage/ProjectMain.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
 import NavBar from '../NavBar/NavBar';
+import './ProjectPage.css';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+};
 
 
 class ProjectPage extends Component {
     render() {
+        const { classes } = this.props;
 
         return (
-            <div>
-                <NavBar />
-            </div>
+            <>
+                <div>
+                    <NavBar />
+                </div>
+                <div className="hero-content">
+                    {/* <div className="github-logo"><img src="/images/GitHub-Mark-Light-120px-plus.png" alt="github logo" /> */}
+                        <Typography variant="h6" color="inherit" className={classes.grow}><h2>KASHIF SIDDIQUI</h2></Typography>
+                    </div>
+
+                
+            </>
         );
     }
 }
+ProjectPage.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
 
 
-export default ProjectPage; 
+export default withStyles(styles)(ProjectPage);
